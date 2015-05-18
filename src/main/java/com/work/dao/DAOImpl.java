2a5e1ef7cc328e7ef29657f858afb51914006a3c
sokyrka <sokyrka_evgeniy@yahoo.com>
2015-05-18 23:41:43 +0300

    changed dao

--- src/main/java/com/work/dao/DAOImpl.java
+++ src/main/java/com/work/dao/DAOImpl.java
@@ -1,55 +1,27 @@
 package com.work.dao;
 
-import com.work.common.Book;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 import javax.sql.DataSource;
-import java.sql.*;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by Eugine Sokirka on 18.05.2015.
  */
 public class DAOImpl  {
-    private final String url = "jdbc:mysql://176.37.217.24:3306/zhenya_test1";
-    private final String user = "quattro";
-    private final String password = "Zhenya2015";
-
-    @Autowired
     private DataSource dataSource;
-    Connection connection = null;
 
-    public DAOImpl() {
+    public DAOImpl(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    public void get(){
+        Connection connection = null;
+
         try {
             connection = dataSource.getConnection();
-            Statement statement = connection.createStatement();
-
-            ResultSet rs = statement.executeQuery("SELECT busy FROM BOOKS");
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-    /*public DAOImpl(){
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
-
-            ResultSet rs = statement.executeQuery("SELECT busy FROM BOOKS");
-            while(rs.next()){
-                //String name = rs.getString("first_name");
-                Boolean busy = rs.getBoolean("busy");
-                //String author = rs.getString("AUTHOR");
-
-                System.out.print(busy);
-                //System.out.println(author);
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }*/
-
     }
+
 }
--- src/main/java/com/work/dao/Main.java
+++ src/main/java/com/work/dao/Main.java
@@ -1,14 +1,16 @@
 package com.work.dao;
 
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Eugine Sokirka on 18.05.2015.
  */
 public class Main {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("web/WEB-INF/applicationContext.xml");
-        DAOImpl dao = new DAOImpl();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-dispatcher-servlet.xml");
+        DataSource dataSource = context.getBean("dataSource", DataSource.class);
+        DAOImpl dao = new DAOImpl(dataSource);
     }
 }
